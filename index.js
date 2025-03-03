2f1e248c4b276ceb8d6fa3a45db2bb536dbf6009
ZoeyNguyen65 <hanhngoc9956@gmail.com>
2025-03-02 22:12:30 -0600

    modify index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerBtn.addEventListener('click', () => {
+    hamburgerMenu.classList.toggle('show-menu');
+    hamburgerBtn.ariaExpanded = hamburgerMenu.classList.contains('show-menu');
+});
+
+document.addEventListener('click', (event) => {
+    if (!hamburgerMenu.contains(event.target) && !hamburgerBtn.contains(event.target)) {
+      hamburgerMenu.classList.remove('show-menu');
+      hamburgerBtn.ariaExpanded = 'false';
+    }
+});
+  
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape' && hamburgerMenu.classList.contains('show-menu')) {
+    hamburgerMenu.classList.remove('show-menu');
+    hamburgerBtn.ariaExpanded = 'false';
+    hamburgerBtn.focus();
+    }
+});
+  
